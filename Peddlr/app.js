8a8af476322d3096972c430cc0d3b0d56d750e4f
Eyre Mamintada <eyre@peso.fun>
2019-04-11 17:16:40 +1000

    edited PORT

--- Peddlr/app.js
+++ Peddlr/app.js
@@ -13,7 +13,7 @@ var routes = require('./routes/routes.js');
 app.use('/',routes);
 
 // Start the server
-app.listen(3000,function(req,res){
-   console.log('Express listening on port 3000');
+app.listen(process.env.port || 3000,function(req,res){
+   console.log('Service listening');
 });
 
