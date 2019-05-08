3e65bc6397cdd0b17e5118cc27ad07b0b2ac7098
Marzuk Amin <marzuka@student.unimelb.edu.au>
2019-05-08 14:00:48 +1000

    Added views path

--- Peddlr/app.js
+++ Peddlr/app.js
@@ -1,8 +1,10 @@
 //Set up express
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
 
 var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
@@ -21,4 +23,3 @@ app.listen(PORT, () => {
    console.log(process.env.PORT);
    console.log(`Listening on ${ PORT }`);
 });
-
